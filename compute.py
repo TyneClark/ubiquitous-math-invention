c2a4c8e8bdba45ea269cfb2dfb9b0b68662b8924
Tyne Clark <111921888+TyneClark@users.noreply.github.com>
2023-05-08 17:33:43 -0600

    Update for loop, as to not duplicate entries

--- compute.py
+++ compute.py
@@ -24,11 +24,11 @@ def startTestingNumbers():
     print("Started Testing Numbers.")
     db = numDB()
     used_list = db.getDB()
-    print("used_list:", used_list)
-    if used_list == []:
-        used_list.append(RealDictRow([('num', 1)]))
+    #print("used_list:", used_list)
+    #if used_list == []:
+        #used_list.append(RealDictRow([('num', 1)]))
     latest_num = (used_list[-1])['num']
-    for i in range(latest_num, latest_num + 1_000_000):
+    for i in range(latest_num + 1, latest_num + 1_000_000):
         testNumber(i)
     startTestingNumbers()
 
